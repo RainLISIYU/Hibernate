f4f16bd41e85313a35d67738ad0a1b94a034749b
rain241 <1455140744@qq.com>
2019-07-30 14:37:05 +0800

    QBC Test

--- src/com/liang/demo5/Hibernate2Test.java
+++ src/com/liang/demo5/Hibernate2Test.java
@@ -3,9 +3,10 @@ package com.liang.demo5;
 import com.liang.po.Customer;
 import com.liang.utils.HibernateUtils;
 import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.criterion.Order;
+import org.hibernate.criterion.*;
 import org.junit.Test;
 
 /**
@@ -30,6 +31,73 @@ public class Hibernate2Test {
             transaction.rollback();
             e.printStackTrace();
         }
+    }
+
+    @Test
+    /**
+     * 条件查询
+     */
+    public void test2(){
+        Session session = HibernateUtils.getCurrentSession("hibernate5.cfg.xml");
+        Transaction transaction = session.beginTransaction();
+
+        Criteria criteria = session.createCriteria(Customer.class);
+
+        try{
+            criteria.add(Restrictions.eq("cust_source", "百度"));
+            System.out.println(criteria.list());
+            transaction.commit();
+        }catch (Exception e){
+            transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    /**
+     * 统计查询
+     */
+    public void test3(){
+        Session session = HibernateUtils.getCurrentSession("hibernate5.cfg.xml");
+        Transaction transaction = session.beginTransaction();
+
+        Criteria criteria = session.createCriteria(Customer.class);
+
+        try{
+            /**
+             * add   :普通的条件
+             * addOrder : 排序
+             * setProjection :聚合函数和group by、having
+             */
+            criteria.setProjection(Projections.rowCount());
+            Object o = criteria.uniqueResult();
+            System.out.println(o);
+            transaction.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
+
+    @Test
+    /**
+     * 离线查询
+     */
+    public void test4(){
+        DetachedCriteria detachedCriteria = DetachedCriteria.forClass(Customer.class);
+        detachedCriteria.add(Restrictions.eq("cust_source", "百度"));
+
+        Session session = HibernateUtils.getCurrentSession("hibernate5.cfg.xml");
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            Criteria criteria = detachedCriteria.getExecutableCriteria(session);
+            System.out.println(criteria.list());
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
 }
